bbc7a11e0fc5746bc0c57a22f2abc23ba823e474
maha <mahabubarmaha@gmail.com>
2024-12-09 23:29:10 +0600

    Leangth

--- src/W3School/StringLeangthOrAnyLeangth.java
+++ src/W3School/StringLeangthOrAnyLeangth.java
@@ -4,5 +4,6 @@ public class StringLeangthOrAnyLeangth {
     public static void main(String[] args) {
         String Maha="Md.Mahabubar Rahaman.";
         System.out.println("The Leangth of Maha: "+ Maha.length());
+        System.out.println("Hi Maha");
     }
 }
