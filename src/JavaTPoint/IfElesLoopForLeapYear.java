6077b66e55b4bc733f9c7fbc02e0b6e9d9dbc10f
maha <mahabubarmaha@gmail.com>
2024-12-10 00:44:27 +0600

    Leap year er another condition

--- src/JavaTPoint/IfElesLoopForLeapYear.java
+++ src/JavaTPoint/IfElesLoopForLeapYear.java
@@ -3,7 +3,7 @@ package JavaTPoint;
 public class IfElesLoopForLeapYear {
     public static void main(String[] args) {
         int year=2020;
-        if(((year % 4 ==0) && (year % 100 !=0)) || (year % 400==0)){
+        if(((year % 4 ==0) && (year % 100 !=0)) || (year % 400==0)){  // if(((year % 4 ==0) ||(year % 400==0)) && (year % 100 !=0))
             System.out.println("LEAP YEAR");
         }
         else{
