bb73eacef43c323815832d40f48b3dca4f6c339b
maha <mahabubarmaha@gmail.com>
2024-12-09 13:36:37 +0600

    maha.java

--- src/AnisulClassCode/maha.java
+++ src/AnisulClassCode/maha.java
@@ -3,6 +3,7 @@ package AnisulClassCode;
 public class maha {
     public static void main(String[] args) {
         System.out.println("mahabuber");
+        System.out.println("tomal");
 
     }
 }
